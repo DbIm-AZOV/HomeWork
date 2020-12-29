95ffed784e1c179ce795e24afe40423d282d8d70
DbIm-AZOV <74107512+DbIm-AZOV@users.noreply.github.com>
2020-12-29 12:46:13 +0200

    Add files via upload

--- task 2.js
+++ task 2.js
@@ -1,6 +1,6 @@
 function palindrome(x) {
   if (typeof x != "number") return alert("not number");
-  if  ((x < ((-2) ** 31)) || (x > (2 ** 31 - 1))) return alert("error")
+  if  ((x < ((-2) ** 31)) || (x > (2 ** 31 - 1))) return alert("Not range")
   
   let numArray = String(x).split("");
   let reversedArray = String(x).split("").reverse();
